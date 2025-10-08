5e757b72b2af99f4715376d1d3a27e30b78ab5dd
emmanuelobinyan <emmanuelobinyan936@gmail.com>
2025-10-08 20:55:47 +0100

    added oomment to appprovider

--- src/config/AppProvider.jsx
+++ src/config/AppProvider.jsx
@@ -6,6 +6,7 @@ export const AppProvider=({children})=>{
       <TabProvider>
      <AuthProvider>
        {children}
+       {/* for the app provider */}
      </AuthProvider>
      </TabProvider>
     )
